12c3bbda5e7ad92f8e1a4ed39c48cdd9881f88b5
Amir Kozhanov <amirkozanov@gmail.com>
2022-09-17 13:30:55 +0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: false
+    }
+});
